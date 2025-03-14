54249abb5cc7b59740b394b9832625be719dfcc4
Maciej Śledziński <maciejs.sledzinski@cyfroweszkoly.pl>
2025-03-14 14:21:44 +0100

    dodanie suwaka do ustawiania ilosci, po zaznaczeniu kawy wyswietla sie odpowiedni obraz, po przesunieciu suwaka wyswietla sie ilosc w tekscie, dodanie kolorow i dostosowanie wygladow

--- app/src/main/java/com/example/kreator_zamawiania_kawy/MainActivity.kt
+++ app/src/main/java/com/example/kreator_zamawiania_kawy/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.kreator_zamawiania_kawy
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val ilosc_text: TextView = findViewById(R.id.ilosc_text)
+        val ilosc: SeekBar = findViewById(R.id.ilosc)
+
+        ilosc_text.text = "${ilosc.progress}"
+
+        ilosc.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                ilosc_text.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+
+
+        val obrazek:ImageView = findViewById(R.id.obrazek)
+        val zamow:Button = findViewById(R.id.zamow)
+        val radioGroup: RadioGroup = findViewById(R.id.rodzaj)
+
+        val images = mapOf(
+            R.id.rodzaj_espresso to R.drawable.espresso,
+            R.id.rodzaj_cappuccino to R.drawable.capuccino,
+            R.id.rodzaj_latte to R.drawable.latte
+        )
+
+        zamow.setOnClickListener {
+
+            val selectedRadioButtonId = radioGroup.checkedRadioButtonId
+
+            if (selectedRadioButtonId != -1) {
+                val selectedImageResId = images[selectedRadioButtonId]
+                if (selectedImageResId != null) {
+                    obrazek.setImageResource(selectedImageResId)
+                }
+            }
+        }
+
+
+
+
     }
 }
